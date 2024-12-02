1d5ebba7df16e330fc34090a187dfdd4d2ff3f9c
bryson <65848252+nyaliti@users.noreply.github.com>
2024-12-02 04:52:19 +0300

    Add setup.py for package configuration and metadata for Genius Bryson

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Genius Bryson',
+    version='1.0.0',
+    packages=find_packages(),
+    description='Automated assistant for MetaTrader 5 to analyze forex charts.',
+    author='Bryson Omullo',
+    author_email='bnyaliti@gmail.com',
+    url='https://github.com/nyaliti/Genius_Bryson',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
